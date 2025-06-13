6c974e91383853c71ef83d83130515ad43b15c43
HeinrichWarkentin <94618491+HeinrichWarkentin@users.noreply.github.com>
2025-06-13 18:08:57 +0200

    Nav Button Animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Memora';
+
+  isChanged = false;
+  toggleNavIcon() {
+    this.isChanged = !this.isChanged;
+  }
 }
